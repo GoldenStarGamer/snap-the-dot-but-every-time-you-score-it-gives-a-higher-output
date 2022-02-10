4615fcae916e16a197029c2d45300676e7e3fbff
GoldenStarGamer <75682000+GoldenStarGamer@users.noreply.github.com>
2022-02-10 15:12:15 +0000

    added debug (activated with button B)

--- main.ts
+++ main.ts
@@ -1,11 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     if (sprite.get(LedSpriteProperty.X) == 2) {
-        game.addScore(10)
+        game.addScore(debug)
     } else {
         game.gameOver()
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    debug = 10
+})
+input.onButtonPressed(Button.B, function () {
+    debug = 1000
+})
 let sprite: game.LedSprite = null
+let debug = 0
+debug = 10
 sprite = game.createSprite(1, 2)
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P0, Math.abs(game.score()))
